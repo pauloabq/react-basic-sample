d442db00b278b7d6b8aa4aa0279788362c53e4b1
pauloabq <pcajunior@gmail.com>
2020-04-07 19:53:15 -0300

    Updates

---
+++ precache-manifest.a89d7390b5a06202c0501201324c6d4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb912a2f8bb1810eb6d660dd175192c8",
+    "url": "/react-basic-sample/index.html"
+  },
+  {
+    "revision": "9d2763934502cb9644fa",
+    "url": "/react-basic-sample/static/css/main.a6c49b6d.chunk.css"
+  },
+  {
+    "revision": "f14c267a1a7dbd8ed8e7",
+    "url": "/react-basic-sample/static/js/2.bddcc92c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basic-sample/static/js/2.bddcc92c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d2763934502cb9644fa",
+    "url": "/react-basic-sample/static/js/main.83a47f38.chunk.js"
+  },
+  {
+    "revision": "6bd19fefd0c672eebd28",
+    "url": "/react-basic-sample/static/js/runtime-main.60d89aff.js"
+  }
+]);
