f12ddaa5015c4b381032cf5d9c33dd29e3585bfd
pauloabq <pcajunior@gmail.com>
2020-04-07 17:25:30 -0300

    Finalizado o app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Table from './Table';
+import Form from './Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{    
+    state = {
+        characters: [],
+    }
+    /*
+    Podemos declarar métodos especiais no componente de classe para executar algum código quando um componente é montado e desmontado
+    Estes métodos são chamados de “métodos de ciclo de vida”.
+    O método é executado depois que a saída do componente é renderizada no DOM
+        componentDidMount()
+        componentWillUnMount()
+        
+    componentDidMount(){
+        console.log('bla');
+    }
+
+    Ao renderizar App, teremos 'bla' no console automaticamente.
+    */    
+    
+    
+    removeCharacter = index => {
+        const { characters } = this.state;
+        this.setState({
+            characters : characters.filter((character, i) => {
+                return i !== index;
+            })
+        })
+    }
+
+
+
+    handleSubmit = character => {
+       this.setState({ characters: [...this.state.characters, character] })
+    }
+
+    render(){
+        const { characters } = this.state;
+        return(
+            <div className="container">
+                <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+                <Form handleSubmit={this.handleSubmit} />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
